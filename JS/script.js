1dd8e78a114a10e4dc7912815ec71dd751cb72d0
Adrinalinio <38666430+Adrinalinio@users.noreply.github.com>
2020-11-27 19:57:36 +0200

    123

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+//function MyArray(a) //{
+//summ = a[0] + a[1] + a[2] + a[3];
+//let summ1 = summ;
+//let summ2 = summ1 / a.length
+//console.log(summ2)
+//}
+
+//b = MyArray([2, 4, 5, 7]);
+
+
+//function Damn(a) {
+//let Full = 0;
+//let Semi = 0;
+//for (let i = 0; i < a.length; i++)
+//if ([i] % 2 == 0) {
+//Full += a[i]
+//} //else {
+//([i] % 2 == 1);
+//Semi += a[i];
+//}
+//let finish = Full - Semi;
+//console.log(finish);
+//}
+
+//b = Damn([2, 5, 7, 9, 10]);
+
+
---
+++ thirdHomework/script.js
@@ -0,0 +1,35 @@
+let a = document.getElementById('change_color');
+let b = document.getElementsByClassName('first')[0];
+a.addEventListener('click', function () {
+    b.classList.toggle('active');
+});
+
+let c = document.getElementById('change_title');
+let d = document.getElementsByTagName('h1')[0];
+c.addEventListener('click', function () {
+    d.classList.toggle('Name')
+    if (d.classList.contains('Name'))
+        d.innerText = 'Kalythuk Dmytro'
+    else
+        d.innerText = 'Title'
+});
+
+let e = document.getElementById('words_counter');
+let r = document.getElementsByClassName('second')[0];
+let f = document.getElementsByClassName('counter')[0];
+r = r.textContent.split(' ');
+r = parseInt(r)
+console.log(r);
+e.addEventListener('click', function () {
+    f += r;
+    
+});
+
+let t = document.getElementById('get_text');
+let y = document.getElementsByClassName('second')[0];
+t.addEventListener('click', function () {
+let l = y.textContent;
+    alert(l);
+    y.removeAttribute[0];
+});
+
